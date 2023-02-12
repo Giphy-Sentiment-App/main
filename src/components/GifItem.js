cacc8fd31d2df611eae1d1a2582f8ef0464e97a0
jhandrahan <jessicahandrahan@gmail.com>
2023-02-12 15:22:32 -0400

    implemented on click when gif is selected and saved to firebase

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -1,7 +1,24 @@
+import firebase from '../firebase';
+import { getDatabase, ref, push } from 'firebase/database';
+import { useState } from 'react';
+
 const GifItem = ({gifData})=> {
+
+  const [userClick, setUserClick] = useState(null);
+
+  const gifClick = () => {
+    
+    const database = getDatabase(firebase);
+    const dbRef = ref(database);
+    push(dbRef, userClick);
+    setUserClick(gifData.images.original.url);
+    // window.location.reload(false);
+  }
+ 
   return (
     <>
-    <img src={gifData.images.original.url} alt={gifData.title} /> </>
+    <img src={gifData.images.original.url} alt={gifData.title} onClick={gifClick}/> 
+    </>
    
   )
 }
