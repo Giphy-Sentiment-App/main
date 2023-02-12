46da5e27be1e280f8409aa305ddbda1c0bf16a10
Michelle <miche.dmnt@gmail.com>
2023-02-12 15:06:01 -0500

    created nested elements in firebase to store on click data

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -5,16 +5,28 @@ import { useState } from 'react';
 const GifItem = ({gifData})=> {
 
   const [userClick, setUserClick] = useState(null);
+  const currentDate = new Date();
+  const options = {year:'numeric', month:'numeric', day:'numeric'}
 
   const gifClick = () => {
     
     const database = getDatabase(firebase);
-    const dbRef = ref(database);
+    const dbRef = ref(database, '/userHistory/gifUrl');
     push(dbRef, userClick);
     setUserClick(gifData.images.original.url);
+
+    const userSavedData = (user) => {
+      const userData = ref(database, '/userHistory/date')
+      push(userData, user)
+    }
+    userSavedData(currentDate.toLocaleString(undefined,options))
+    // clear page on click function needed to be placed correctly
     // window.location.reload(false);
+
+    console.log(gifData);
   }
  
+
   return (
     <>
     <img src={gifData.images.original.url} alt={gifData.title} onClick={gifClick}/> 
--- src/components/GifResult.js
+++ src/components/GifResult.js
@@ -2,18 +2,18 @@
 import GifItem from "./GifItem";
 
 const GifResult = ({gifs, emptyInput}) => {
-  console.log(gifs)
   return (
-      <>
+    <>
       <ul>
         {
           emptyInput ? <p>Please tell us how you are feeling today</p>
-        
-
-        
-      : (gifs.map((gifObject)=>{
-      return <li key={gifObject.id}> <GifItem gifData={gifObject}/> </li>
-      }))
+          
+          
+          
+          : (gifs.map((gifObject)=>{
+            // console.log(gifObject);
+            return <li key={gifObject.id}> <GifItem gifData={gifObject}/> </li>
+          }))
     }
       </ul>
       
