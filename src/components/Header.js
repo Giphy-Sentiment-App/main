4dc0dfde7fa6da08352319286ca0d7b0bf95f880
jhandrahan <jessicahandrahan@gmail.com>
2023-02-23 21:13:56 -0400

    added styles to header and form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,29 @@
 import logo from '../assets/logo.jpg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   // created variables for the date to appear as the same as the browser locale, with options to how to be displayed
   const currentDate = new Date();
   const options = {year:'numeric', month:'numeric', day:'numeric'}
   return (
-    <>
-    <div className="imgContainer">
-    <img src={logo} alt="Giphy Sentiment Logo" width={150}/>
-    </div>
+    <header>
     <div className="wrapper">
+    <div className="imgContainer">
+      <img src={logo} alt="Giphy Sentiment Logo"/>
       <h1>{currentDate.toLocaleString(undefined,options)}</h1>
+        <div className="moodHistoryContainer wrapper">
+          <Link to="/MoodHistory">
+            <i class="fa-solid fa-shield-heart"></i>
+            <div className='moodHistory'>Mood History</div>
+          </Link>
+        </div>
+    </div>
     <h2>Please describe your emotional state with ONE word.</h2>
 
     </div>
     
     
-    </>
+    </header>
     
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,16 @@
 import React from 'react'
 import Header from './Header';
 import Form from './Form';
-import { Link } from 'react-router-dom';
+
 
 
 
 const Home = () =>{
   return (
-    <>
+    <main>
     <Header />
     <Form />
-    <div className="moodHistoryContainer wrapper">
-        <Link to="/MoodHistory">
-          <button className='moodHistory'>Mood History</button>
-        </Link>
-    </div>
-    </>
+    </main>
   )
 }
 export default Home;
